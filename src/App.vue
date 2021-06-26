e4b4aef4218e858fd0d4e56633195591c0470f8b
marongiu-ai <82757746+marongiu-ai@users.noreply.github.com>
2021-06-26 12:15:03 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -168,16 +168,7 @@ a:hover {
   }
 }
 
-.sr-only {
-  position: absolute;
-  width: 1px;
-  height: 1px;
-  padding: 0;
-  margin: -1px;
-  overflow: hidden;
-  clip: rect(0, 0, 0, 0);
-  border: 0;
-}
+
 
 .shuffle-card-move {
   transition: transform 0.8s ease-in;
