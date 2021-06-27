aca6efb61827656e38035d7303a4194bf90b6ed3
marongiu-ai <82757746+marongiu-ai@users.noreply.github.com>
2021-06-27 17:27:04 +0200

    Update Card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -99,7 +99,9 @@ export default {
   
   background-image: url('/images/back.png');
   transform: translate(0%,-4%);
-  border: 2px solid rgba(99,195,40,1);  
+  border: 2px solid rgba(99,195,40,1); 
+  color: rgba(255, 255, 255, 1);
+  box-shadow: 0 5px 15px rgba(145, 92, 182, .4);
 }
 
 
