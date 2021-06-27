71b05b8db68bea9d360e3d05fb881fb6762fc66a
marongiu-ai <82757746+marongiu-ai@users.noreply.github.com>
2021-06-27 18:09:23 +0200

    Update Card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -98,13 +98,12 @@ export default {
 .card-face.is-back:hover {
   transform: translate(0%,-4%);
   border: 2px solid rgba(99,195,40,1);
-  box-shadow: 15 15 30px rgba(145, 92, 182, .4);
-  box-shadow: -15 -15 30px rgba(145, 92, 182, .4);
+  box-shadow: 15 15 30px rgba(145, 92, 182, .4),-15 -15 30px rgba(145, 92, 182, .4);
+  
 }
 .card-image:hover{
   color: rgba(255, 255, 255, 1);
-  box-shadow: 15 15 30px rgba(145, 92, 182, .4);
-  box-shadow: -15 -15 30px rgba(145, 92, 182, .4);
+  box-shadow: 15 15 30px rgba(145, 92, 182, .4),-15 -15 30px rgba(145, 92, 182, .4);
 }
 
 
